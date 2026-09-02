f7b525959705a5988f9f8ee4d2babb09daa66e0e
nanashi <cibicco323@gmail.com>
2026-09-02 11:51:55 +0900

    chore:ジャンル機能の雛形を追加(モデル･マイグレーション･FormRequest･シーダー･ファクトリ)

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreGenreRequest;
+use App\Http\Requests\UpdateGenreRequest;
+use App\Models\Genre;
 
 class GenreController extends Controller
 {
@@ -25,7 +27,7 @@ class GenreController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreGenreRequest $request)
     {
         //
     }
@@ -33,7 +35,7 @@ class GenreController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Genre $genre)
     {
         //
     }
@@ -41,7 +43,7 @@ class GenreController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Genre $genre)
     {
         //
     }
@@ -49,7 +51,7 @@ class GenreController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateGenreRequest $request, Genre $genre)
     {
         //
     }
@@ -57,7 +59,7 @@ class GenreController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Genre $genre)
     {
         //
     }
