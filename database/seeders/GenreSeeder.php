0f4de297d6c8c2e79770f763d60cfb68b2320999
nanashi <cibicco323@gmail.com>
2026-09-02 14:33:17 +0900

    feat:GenreSeeder実装

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Genre;
 use Illuminate\Database\Seeder;
 
 class GenreSeeder extends Seeder
@@ -11,6 +12,20 @@ class GenreSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $genres = [
+            '小説',
+            'ビジネス',
+            '技術書',
+            '自己啓発',
+            'エッセイ',
+            '歴史',
+            '科学',
+            '芸術',
+            '料理',
+            '旅行',
+        ];
+        foreach ($genres as $genre) {
+            Genre::firstOrCreate(['name' => $genre]);
+        }
     }
 }
